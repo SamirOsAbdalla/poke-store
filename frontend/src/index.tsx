75d84bb3a92ed5b651582bf72f0c3609879efcb4
samir01 <samir.os.abdalla@gmail.com>
2022-11-03 17:56:36 -0700

    Added dependencies and modified exports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App />
+  //Wrapping app in order to actually
+  //use routes
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
