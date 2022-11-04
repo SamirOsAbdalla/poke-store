c355064115e7c953bffca74f2434f6132fb19df5
samir01 <samir.os.abdalla@gmail.com>
2022-11-03 19:36:06 -0700

    Added temporary paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import { Navbar } from './components/Navbar/navbar';
+import { Greeting } from './components/Greeting/greeting';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Greeting />
+      </div>
+      <Routes>
+        <Route path="/store" element={<Navbar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
