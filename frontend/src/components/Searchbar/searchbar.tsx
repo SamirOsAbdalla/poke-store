bedebc1d2c083a6c8bff143ab48a85a2bcea936e
samir01 <samir.os.abdalla@gmail.com>
2022-11-04 13:35:56 -0700

    Added some logic to obtain value of input

--- frontend/src/components/Searchbar/searchbar.tsx
+++ frontend/src/components/Searchbar/searchbar.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import "./searchbar.css"
+import { useState } from 'react'
 
 
+//This component will search for a specific
+//pokemon
 
 export const Searchbar = () => {
+
+    const [searchedPokemon, setSearchedPokemon] = useState("")
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault()
+    }
+
+    const handlePokemonChange = (e: React.FormEvent<HTMLInputElement>) => {
+        setSearchedPokemon(e.currentTarget.value)
+    }
+
     return (
         <div className="searchbar-wrapper">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Find a pokemon below or click shop!</label>
-                <input
-                    type="text"
-                />
+                <input type="text" value={searchedPokemon} onChange={handlePokemonChange} />
                 <button>Search</button>
             </form>
         </div>
