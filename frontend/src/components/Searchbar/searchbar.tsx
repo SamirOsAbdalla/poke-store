e176a8973c6f781b3c8b387e60ab41829bde2cf8
samir01 <samir.os.abdalla@gmail.com>
2022-11-05 19:07:19 -0700

    When button submits user will now go to a page dedicated to the searched pokemon

--- frontend/src/components/Searchbar/searchbar.tsx
+++ frontend/src/components/Searchbar/searchbar.tsx
@@ -3,6 +3,7 @@ import "./searchbar.css"
 import { useState, useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { updatePokemonName } from "../../slices/search/searchSlice"
+import { useNavigate } from 'react-router-dom';
 //This component will search for a specific
 //pokemon
 
@@ -13,8 +14,11 @@ export const Searchbar = () => {
         e.preventDefault()
     }
 
+
     const currentSearchedPokemon = useAppSelector((state) =>
         state.search.pokemonName)
+
+
 
     useEffect(() => {
 
@@ -23,18 +27,36 @@ export const Searchbar = () => {
         setSearchedPokemon(currentSearchedPokemon)
     }, [])
 
+
+
     const dispatch = useAppDispatch()
 
-    const handlePokemonChange = (e: React.FormEvent<HTMLInputElement>) => {
+
+
+    const handlePokemonNameChange = (e: React.FormEvent<HTMLInputElement>) => {
         setSearchedPokemon(e.currentTarget.value)
     }
+
+
+
+    const navigate = useNavigate()
+    const submitUpdatedName = (e: React.SyntheticEvent) => {
+        e?.preventDefault()
+        const lowercaseSearchedPokemon = searchedPokemon.toLowerCase()
+        dispatch(updatePokemonName(lowercaseSearchedPokemon))
+        navigate(`/shop/${lowercaseSearchedPokemon}`)
+    }
+
+
 
     return (
         <div className="searchbar-wrapper">
             <form onSubmit={handleSubmit}>
                 <label>Find a pokemon below or click shop!</label>
-                <input type="text" value={searchedPokemon} onChange={handlePokemonChange} />
-                <button onClick={() => dispatch(updatePokemonName(searchedPokemon))}>Search</button>
+                <input type="text" value={searchedPokemon} onChange={handlePokemonNameChange} />
+                <button type="submit" onClick={submitUpdatedName}>
+                    Search
+                </button>
             </form>
         </div>
     )
