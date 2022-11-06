de7fc23225fa779aded893d4d93b0e96b6e0aaa3
samir01 <samir.os.abdalla@gmail.com>
2022-11-05 19:05:57 -0700

    Added new path for individually searched pokemon names

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Navbar } from './components/Navbar/navbar';
 import { Greeting } from './components/Greeting/greeting';
 import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { useEffect } from "react"
+import { Pokepage } from './components/Pokepage/Pokepage';
+import { useAppSelector } from './app/hooks'
 
 function App() {
+
+  const currentPokemon = useAppSelector(state => state.search.pokemonName)
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -11,6 +17,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Greeting />} />
+        <Route path="/shop/:pokemon_name" element={<Pokepage name={currentPokemon} />} />
       </Routes>
     </BrowserRouter>
   );
