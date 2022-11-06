ffe362f263ba4f442a430b4136818391154d9db1
samir01 <samir.os.abdalla@gmail.com>
2022-11-05 18:01:40 -0700

    Wrapped app in provider in order to use store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
-=======
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from 'react-redux';
 import store from './app/store';
->>>>>>> landingPage
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-<<<<<<< HEAD
-  //Wrapping app in order to actually
-  //use routes
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-=======
   <Provider store={store}>
     <App />
   </Provider>
->>>>>>> landingPage
 );
