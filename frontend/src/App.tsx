b0f04947320b72cf16fb78a994c8921a9a889aa1
samir01 <samir.os.abdalla@gmail.com>
2022-11-07 18:12:10 -0800

    React now remembers the current searched pokemon on reload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,30 +6,46 @@ import { useEffect } from "react"
 import { Pokepage } from './components/Pokepage/Pokepage';
 import { useAppSelector } from './app/hooks'
 import axios from 'axios';
+import { useAppDispatch } from './app/hooks'
+import { updatePokemonName } from "./slices/search/searchSlice"
 
-function App() {
+const App = () => {
 
   const currentPokemon = useAppSelector(state => state.search.pokemonName)
+  const dispatch = useAppDispatch()
+
 
 
   //fetches all pokemon names
   const getPokemonNames = async () => {
     const results = await axios.get("http://pokeapi.co/api/v2/pokemon/?limit=905")
       .then(response => response.data.results)
-
     const nameMap = new Map();
 
     for (let i = 0; i < results.length; i++) {
       nameMap.set(results[i].name, results[i])
     }
-
     window.localStorage.setItem("ALL_POKEMON_NAMES", JSON.stringify(Object.fromEntries(nameMap)))
     return results
   }
 
+
+
+  const setCurrentPokemonSearched = (currentPokemon: string) => {
+    dispatch(updatePokemonName(currentPokemon))
+  }
+
+
+
   useEffect(() => {
     if (window.localStorage.getItem("IS_POKEMON_STORED")) {
-      //access stored pokemon
+
+
+      //react now remembers the current pokemon page on reload
+      const currentPokemonSearched = window.localStorage.getItem("CURRENT_SEARCHED_POKEMON")
+      if (currentPokemonSearched) {
+        setCurrentPokemonSearched(currentPokemonSearched)
+      }
     }
     else {
       //get pokemon from API initially
@@ -39,6 +55,9 @@ function App() {
       getPokemonNames()
     }
   }, [])
+
+
+
 
   return (
     <BrowserRouter>
