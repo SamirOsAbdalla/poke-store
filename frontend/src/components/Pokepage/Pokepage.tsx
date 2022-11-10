afe72a305181981cb16a16bf0af1d1a579d0a116
samir01 <samir.os.abdalla@gmail.com>
2022-11-09 19:10:34 -0800

    Pokepage now receives the current searched pokemon from the route parameters

--- frontend/src/components/Pokepage/Pokepage.tsx
+++ frontend/src/components/Pokepage/Pokepage.tsx
@@ -2,23 +2,25 @@ import React from 'react'
 import axios from "axios"
 import { useEffect, useState } from "react"
 import retrievePokemon from "../../services/pokemon"
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { useParams } from 'react-router-dom';
 
 type pokemonName = {
     name: string
 }
 
-export const Pokepage = ({ name }: pokemonName) => {
-
+export const Pokepage = () => {
+    const name = useParams().pokemon_name as string
     useEffect(() => {
         retrievePokemon.getPokepage(name).then(response => {
             console.log(response)
         })
-    }, [])
+    })
 
     return (
         <div>
             <div>{name}</div>
-        </div>
+        </div >
 
     )
 }
