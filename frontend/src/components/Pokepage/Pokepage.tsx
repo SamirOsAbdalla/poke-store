83957fa679c86d47bfc584aac4d3a4ca46fe0db8
samir01 <samir.os.abdalla@gmail.com>
2022-11-11 20:21:33 -0800

    Pokepage now type narrows depending on response type

--- frontend/src/components/Pokepage/Pokepage.tsx
+++ frontend/src/components/Pokepage/Pokepage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from "axios"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import pokemonService from "../../services/pokemon"
 import { useParams } from 'react-router-dom';
 import { pokemonInfo, errorMessage } from '../../interfaces/interface'
@@ -8,25 +8,46 @@ import { pokemonInfo, errorMessage } from '../../interfaces/interface'
 
 
 export const Pokepage = () => {
+
+    const [currentPokemonPage, setCurrentPokemonPage] = useState<pokemonInfo>({
+        id: 0,
+        stats: [],
+        officialArtwork: "",
+        types: [],
+        abilities: [],
+        height: 0,
+        weight: 0,
+        kind: ""
+    })
+
     const name = useParams().pokemon_name as string
 
     const fetchPokemon = async () => {
 
-        const res = await pokemonService
+        const pokemon = await pokemonService
             .getPokepage(name)
             .then((response: errorMessage | pokemonInfo) => {
                 return response
             })
+
+        if (pokemon.kind === "pokemon") {
+            const narrowedPokemon: pokemonInfo = pokemon as pokemonInfo
+            setCurrentPokemonPage(narrowedPokemon)
+        } else {
+        }
+
     }
 
 
     useEffect(() => {
         fetchPokemon()
-    })
+    }, [])
 
     return (
         <div>
-            <div>{name}</div>
+            <div>
+                {name}
+            </div>
         </div >
 
     )
