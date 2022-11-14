75cfc0b61c576f0a24aae32fe19e7987c871ff2f
samir01 <samir.os.abdalla@gmail.com>
2022-11-13 17:53:18 -0800

    Changed caching package as well as changed post to get using new wrapper api

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,29 @@
 import express, { Express, Request, Response } from "express"
+import { AxiosResponse, AxiosError } from "axios"
+import { PokemonClient, Pokemon } from 'pokenode-ts';
 const app = express()
 const cors = require('cors')
 const axios = require("axios")
-import { AxiosResponse, AxiosError } from "axios"
-const apicache = require("apicache");
+
 
 app.use(cors())
 app.use(express.json());
 
-app.get('/shop', (req: Request, res: Response) => {
-    res.json("In shop")
+app.get('/', async (req: Request, res: Response) => {
+    const api = new PokemonClient({
+        cacheOptions: { maxAge: 500000, exclude: { query: false } },
+    })
+
+    const allPokemon = await api.listPokemons(0, 1154).then(response => {
+        return response;
+    })
+
+    res.send(allPokemon)
 })
 
 interface pokemonInfo {
     id: number,
     stats: Array<any>,
-    officialArtwork: string,
     types: Array<any>,
     abilities: Array<any>,
     height: number,
@@ -23,27 +31,24 @@ interface pokemonInfo {
     kind: string,
     name: string
 }
-let cache = apicache.middleware
 
-//caching all routes for 1 hour
-app.use(cache('1 hour'))
+app.get("/shop/:pokemon_name", async (req: Request, res: Response) => {
+    const api = new PokemonClient({
+        cacheOptions: { maxAge: 500000, exclude: { query: false } },
+    })
 
-app.get('/shop/:pokemon_name', (req: Request, res: Response) => {
-    const pokemon_name = req.params.pokemon_name
-})
+    //fetches name from path parameters
+    const pokemonName = req.params['pokemon_name']
 
-
-app.post("/shop/:pokemon_name", async (req: Request, res: Response) => {
-    const pokemonURL = req.body.pokemonURL
     try {
-        const fetchedPokemon = await axios.get(pokemonURL).then((response: AxiosResponse) => {
-            return response.data
-        })
+        const fetchedPokemon = await api.getPokemonByName(pokemonName)
+            .then((responsePokemon: Pokemon) => {
+                return responsePokemon
+            })
 
         const responseObject: pokemonInfo = {
             id: fetchedPokemon.id,
             stats: fetchedPokemon.stats,
-            officialArtwork: fetchedPokemon.sprites.other["official-artwork"]["front_default"],
             types: fetchedPokemon.types,
             abilities: fetchedPokemon.abilities,
             height: fetchedPokemon.height,
@@ -54,9 +59,7 @@ app.post("/shop/:pokemon_name", async (req: Request, res: Response) => {
         res.send(responseObject)
     }
     catch (error: unknown) {
-        if (error instanceof AxiosError) {
-            return res.status(400).end()
-        }
+        return res.status(400).end()
     }
 
 })
