c8c8e257cac8ac714141321cce1a5f4a2053d5bd
samir01 <samir.os.abdalla@gmail.com>
2022-11-22 14:58:24 -0800

    Created very minimal toggle that displays the added cart items

--- frontend/src/components/Navbar/navbar.tsx
+++ frontend/src/components/Navbar/navbar.tsx
@@ -2,39 +2,49 @@ import React from "react";
 import "./navbar.css"
 import { NavLink } from "react-router-dom"
 import { useAppSelector } from "../../app/hooks";
-
+import { Cart } from "../Cart/Cart";
 
 export const Navbar = () => {
     const numInCart = useAppSelector(state => {
         return state.numInCart.numberInCart
     })
+
+    const toggleNavbar = () => {
+        console.log("hi")
+        document.querySelector(".toggle-unshow")?.classList.toggle(("toggle-show"))
+    }
     return (
-        <div className="navbar-wrapper">
+        <div className="navbar-wrapper" onClick={toggleNavbar}>
             <div className="navbar-left">
                 <NavLink to="/" className="nav-home link">
                     PokeStore
                 </NavLink>
             </div>
             <div className="navbar-right">
-                <div className="nav-section nav-section-1">
-                    <NavLink to="/shop" className="nav-shop link">
-                        Shop
-                    </NavLink>
+                <div className="navbar-right-top">
+                    <div className="nav-section nav-section-1">
+                        <NavLink to="/shop" className="nav-shop link">
+                            Shop
+                        </NavLink>
+                    </div>
+                    <div className="nav-section nav-section-2">
+                        <NavLink to="/about" className="nav-fav link">
+                            Favorites
+                        </NavLink>
+                    </div>
+                    <div className="nav-section nav-section-3">
+                        <NavLink to="/cart" className="nav-cart link">
+                            Cart {numInCart}
+                        </NavLink>
+                    </div>
+                    <div className="nav-section nav-section-4">
+                        <NavLink to="/login" className="nav-login link">
+                            Log In
+                        </NavLink>
+                    </div>
                 </div>
-                <div className="nav-section nav-section-2">
-                    <NavLink to="/about" className="nav-fav link">
-                        Favorites
-                    </NavLink>
-                </div>
-                <div className="nav-section nav-section-3">
-                    <NavLink to="/cart" className="nav-cart link">
-                        Cart
-                    </NavLink>
-                </div>
-                <div className="nav-section nav-section-4">
-                    <NavLink to="/login" className="nav-login link">
-                        Log In
-                    </NavLink>
+                <div className="toggle-unshow toggle-show">
+                    <Cart />
                 </div>
             </div>
         </div >
