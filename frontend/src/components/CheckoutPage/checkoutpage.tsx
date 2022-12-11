36bd822cff823c2197027a056a3a036613c28219
samir01 <samir.os.abdalla@gmail.com>
2022-12-11 10:08:11 -0800

    Created quick checkout page that displays added cart items

---
+++ frontend/src/components/CheckoutPage/checkoutpage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./checkoutpage.css"
+import { Cart } from '../Cart/Cart'
+
+export const CheckoutPage = () => {
+    return (
+        <div>
+            <Cart />
+        </div>
+    )
+}
