188eed74725d03d0107692aaba8a8ddb369ed653
samir01 <samir.os.abdalla@gmail.com>
2022-12-12 10:45:43 -0800

    Created pokeball image

---
+++ frontend/src/components/Pokeball/Pokeball.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Pokeball.css"
+
+export const Pokeball = () => {
+    return (
+        <div className="pokeball__wrapper">
+            <div className='pokeball'>
+
+            </div>
+        </div>
+    )
+}
