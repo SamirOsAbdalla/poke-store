b53671213d01881abb4eb967caf244bbeb78b588
samir01 <samir.os.abdalla@gmail.com>
2022-12-14 10:13:49 -0800

    changed prop name to something more sensible

--- frontend/src/components/CartSection/CartSection.tsx
+++ frontend/src/components/CartSection/CartSection.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./CartSection.css"
 
-interface ok {
+interface PropType {
     name: string
 }
-export const CartSection = (props: ok) => {
+export const CartSection = (props: PropType) => {
     return (
         <div className="cart-section">{props.name}</div>
     )
