fde0639c52881350a0ba40e694e5a6b22f5e4660
samir01 <samir.os.abdalla@gmail.com>
2022-12-16 08:51:11 -0800

    CartButton now updates state when clicked and adds items to cart

--- frontend/src/components/ShoppingButtons/CartButton.tsx
+++ frontend/src/components/ShoppingButtons/CartButton.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import "./CartButton.css"
 import { AiOutlineShopping } from 'react-icons/ai'
+import { storeNewPokemon } from '../../slices/storedCartPokemon/storedCartPokemon'
+import { increaseNumberInCart } from '../../slices/numInCart/numInCartSlice'
+import { useEffect } from 'react'
 
-export const CartButton = () => {
+
+import { useAppDispatch } from '../../app/hooks'
+
+interface PropType {
+    name: string,
+    sprite: string,
+    price: string
+}
+
+export const CartButton = (props: PropType) => {
+
+    const dispatch = useAppDispatch()
+
+    const addPokemonToCart = () => {
+        const pokemon = {
+            name: props.name,
+            sprite: props.sprite,
+            quantity: 1,
+            price: props.price
+        }
+
+        dispatch(storeNewPokemon(pokemon))
+        dispatch(increaseNumberInCart((1)))
+
+        const cartPokemon = window.localStorage.getItem("")
+    }
+
     return (
         <div>
-            <button className="pokemon__cart__button">
+            <button onClick={() => addPokemonToCart()} className="pokemon__cart__button">
                 <AiOutlineShopping className="shopping__button__icon" />
             </button>
         </div>
