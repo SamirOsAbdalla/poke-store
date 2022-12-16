68c39479cc5c39dc9648864fe020417f6ac3b382
samir01 <samir.os.abdalla@gmail.com>
2022-12-16 08:47:48 -0800

    CartSection now displays the necessary information for a cart item

--- frontend/src/components/CartSection/CartSection.tsx
+++ frontend/src/components/CartSection/CartSection.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import "./CartSection.css"
+import { useEffect, useState } from 'react'
 
 interface PropType {
-    name: string
+    name: string,
+    sprite: string,
+    quantity: number,
+    price: string
 }
 export const CartSection = (props: PropType) => {
+
+    const [updatedPrice, setUpdatedPrice] = useState<string>("")
+
+    useEffect(() => {
+        setUpdatedPrice("$" + (parseFloat(props.price.substring(1)) * props.quantity).toFixed(2))
+    }, [])
+
     return (
-        <div className="cart-section">{props.name}</div>
+        <div className="cart__section__wrapper">
+            <div className='cart__section__top'>
+                <div className='cart__section__image'>
+                    <img src={props.sprite} />
+                </div>
+                <div className='cart__section__name'>
+                    {props.name}
+                </div>
+                <div>
+                    {updatedPrice}
+                </div>
+            </div>
+            <div className='cart__section__quantity'>
+                {props.quantity}
+            </div>
+        </div>
     )
 }
