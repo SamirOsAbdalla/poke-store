fa84269e06b00331c1b8bf02220245f0ecf45f63
samir01 <samir.os.abdalla@gmail.com>
2022-12-16 08:50:38 -0800

    Each pokemon card is supplied with the necessary information now from shop

--- frontend/src/components/Shop/Shop.tsx
+++ frontend/src/components/Shop/Shop.tsx
@@ -6,49 +6,17 @@ import { PokemonCard } from '../PokemonCard/PokemonCard'
 import { pokemonInfo } from '../../interfaces/interface'
 import pokemonService from '../../services/pokemon'
 import "./Shop.css"
-/*type pokemonType = { name: string, url: string }
-type pokemonCard = Pick<pokemonInfo, "name" | "sprite">
+
+
+type pokemonType = {
+    name: string,
+    sprite: string,
+    price: string
+}
 
 export const Shop = () => {
 
     const [loading, setIsLoading] = useState<boolean>(true)
-    const [pokemonCardAttributes, setPokemonCardAttributes] = useState<pokemonCard[]>([])
-    useEffect(() => {
-        const allNames = window.localStorage.getItem("ALL_POKEMON_NAMES")
-        let pokemonNameArray;
-        let allPokemonCardAttributes: pokemonCard[] = [];
-        const getAllPokemonCardAttributes = async (pokeNameArray: pokemonType[]) => {
-            pokeNameArray.forEach(async (pokemon) => {
-                const response = await pokemonService.getPokepage(pokemon.name).then((response: errorMessage | pokemonInfo) => {
-                    return response
-                })
-
-                if (response.kind === "pokemon") {
-                    const narrowedPokemon: pokemonInfo = response as pokemonInfo
-                    const finalPoke: pokemonCard = narrowedPokemon
-                    allPokemonCardAttributes.push(finalPoke)
-                }
-            })
-        }
-
-        if (allNames != null) {
-            pokemonNameArray = JSON.parse(allNames)
-            getAllPokemonCardAttributes(pokemonNameArray)
-            setPokemonCardAttributes(allPokemonCardAttributes)
-        }
-
-    }, [])*/
-
-
-type pokemonType = Pick<pokemonInfo, "name" | "sprite">
-
-export const Shop = () => {
-
-    const [loading, setIsLoading] = useState<boolean>(true)
-
-    useEffect(() => {
-
-    }, [])
 
 
     const allNames = window.localStorage.getItem("ALL_POKEMON_NAMES")
@@ -59,7 +27,7 @@ export const Shop = () => {
     return (
         <div className="shop__wrapper">
             {nameArray.map((pokemon: pokemonType, i: number) => {
-                return <PokemonCard key={i} name={pokemon.name} sprite={pokemon.sprite} />
+                return <PokemonCard key={i} name={pokemon.name} sprite={pokemon.sprite} price={pokemon.price} />
             })}
         </div>
 
