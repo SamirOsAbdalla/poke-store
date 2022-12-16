a7a25ed64404999453b829cce3d8d9b8305c5d88
samir01 <samir.os.abdalla@gmail.com>
2022-12-16 08:49:09 -0800

    Replaced regular links with provided ones from react router in order to prevent page reload and state refreshing

--- frontend/src/components/PokemonCard/PokemonCard.tsx
+++ frontend/src/components/PokemonCard/PokemonCard.tsx
@@ -4,30 +4,32 @@ import { pokemonInfo } from '../../interfaces/interface'
 import { AiOutlineShopping, AiOutlineStar } from 'react-icons/ai'
 import { CartButton } from '../ShoppingButtons/CartButton'
 import { FavButton } from '../ShoppingButtons/FavButton'
+import { Link } from 'react-router-dom'
 
-type CardPropType = Pick<pokemonInfo, "name" | "sprite">
-
-const handleButtonClick = () => {
-    console.log("hi")
+type CardPropType = {
+    name: string,
+    sprite: string,
+    price: string
 }
+
 export const PokemonCard = (props: CardPropType) => {
 
     const pokeName = props.name.toLowerCase()
     const hrefLink = `/shop/${pokeName}`
     return (
         <div className='card'>
-            <a href={hrefLink} className='pokemon__name'>
+            <Link to={hrefLink} className='pokemon__name'>
                 {props.name}
-            </a>
+            </Link>
             <div className="pokemon__image">
                 <img src={props.sprite} />
             </div>
             <div className="pokemon__price">
-                $19.95
+                {props.price}
             </div>
             <div className="pokemon__buttons">
-                <CartButton />
-                <FavButton />
+                <CartButton name={props.name} sprite={props.sprite} price={props.price} />
+                <FavButton name={props.name} sprite={props.sprite} />
             </div>
         </div>
     )
