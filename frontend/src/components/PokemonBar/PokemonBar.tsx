b8d68a031065867e427b244557c49b034aa1b8f8
samir01 <samir.os.abdalla@gmail.com>
2022-12-15 19:19:47 -0800

    Changed number of divs that that format of stats chart works better at smaller screen sizes

--- frontend/src/components/PokemonBar/PokemonBar.tsx
+++ frontend/src/components/PokemonBar/PokemonBar.tsx
@@ -1,19 +1,25 @@
 import "./PokemonBar.css"
-
+import { useEffect } from "react"
 import React from 'react'
+import { NONAME } from "dns"
 
 interface PropType {
     statType: string,
     statValue: number,
     color: string
 }
+
+
 export const PokemonBar = (props: PropType) => {
+
     return (
         <div className="entire__stat__display">
-            <h3 className="stat__type">{props.statType}</h3>
-            <h3 className="stat__value">{props.statValue}</h3>
-            <div className="stats__container">
-                <div style={{ width: props.statValue * 1.4, backgroundColor: props.color }} className="stats HP">
+            <div className="stat__nums">
+                <h3 className="stat__type">{props.statType}</h3>
+                <h3 className="stat__value">{props.statValue}</h3>
+            </div>
+            <div className={`stats__container ${props.statType}`}>
+                <div style={{ width: props.statValue, backgroundColor: props.color }} className={`stats`}>
 
                 </div>
             </div>
