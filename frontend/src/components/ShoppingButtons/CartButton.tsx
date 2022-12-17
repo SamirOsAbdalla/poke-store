c6a469a59e1663b503cf2fb9eed101bd864b0513
samir01 <samir.os.abdalla@gmail.com>
2022-12-17 11:17:44 -0800

    Cart button now adds more than 1 pokemon depending on its passed in value

--- frontend/src/components/ShoppingButtons/CartButton.tsx
+++ frontend/src/components/ShoppingButtons/CartButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./CartButton.css"
-import { AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { storeNewPokemon } from '../../slices/storedCartPokemon/storedCartPokemon'
 import { increaseNumberInCart } from '../../slices/numInCart/numInCartSlice'
 import { useEffect } from 'react'
@@ -11,7 +11,8 @@ import { useAppDispatch } from '../../app/hooks'
 interface PropType {
     name: string,
     sprite: string,
-    price: string
+    price: string,
+    quantity: number,
 }
 
 export const CartButton = (props: PropType) => {
@@ -22,20 +23,19 @@ export const CartButton = (props: PropType) => {
         const pokemon = {
             name: props.name,
             sprite: props.sprite,
-            quantity: 1,
+            quantity: props.quantity,
             price: props.price
         }
 
         dispatch(storeNewPokemon(pokemon))
-        dispatch(increaseNumberInCart((1)))
+        dispatch(increaseNumberInCart((props.quantity)))
 
-        const cartPokemon = window.localStorage.getItem("")
     }
 
     return (
         <div>
             <button onClick={() => addPokemonToCart()} className="pokemon__cart__button">
-                <AiOutlineShopping className="shopping__button__icon" />
+                <AiOutlineShoppingCart className="shopping__button__icon" />
             </button>
         </div>
     )
