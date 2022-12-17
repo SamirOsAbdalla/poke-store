0db183c730296ad8f1633b65cb9278ff41206c13
samir01 <samir.os.abdalla@gmail.com>
2022-12-17 11:13:29 -0800

    Cart section now displays each individual pokemon with all the features to modify the cart if needed

--- frontend/src/components/CartSection/CartSection.tsx
+++ frontend/src/components/CartSection/CartSection.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./CartSection.css"
 import { useEffect, useState } from 'react'
+import { useAppDispatch } from '../../app/hooks'
+import { removePokemonFromCart } from '../../slices/storedCartPokemon/storedCartPokemon'
+import { decreaseNumberInCart } from '../../slices/numInCart/numInCartSlice'
+import { QuantityCounter } from '../QuantityCounter/QuantityCounter'
 
 interface PropType {
     name: string,
@@ -12,26 +16,40 @@ export const CartSection = (props: PropType) => {
 
     const [updatedPrice, setUpdatedPrice] = useState<string>("")
 
+
     useEffect(() => {
         setUpdatedPrice("$" + (parseFloat(props.price.substring(1)) * props.quantity).toFixed(2))
-    }, [])
+    })
+
 
     return (
-        <div className="cart__section__wrapper">
-            <div className='cart__section__top'>
-                <div className='cart__section__image'>
-                    <img src={props.sprite} />
+        <tr>
+            <td className="product__cell">
+                <div className="cart__image__container">
+                    <img src={props.sprite} className="product__sprite" />
                 </div>
-                <div className='cart__section__name'>
+                <div className="product__name">
                     {props.name}
                 </div>
-                <div>
-                    {updatedPrice}
+            </td>
+            <td className="price__cell">
+                {props.price}
+            </td>
+            <td className="quantity__cell">
+                <div className="quantity__cell__container">
+                    <QuantityCounter setQuantity={(quantity) => { }} initialQuantity={"" + props.quantity} />
                 </div>
-            </div>
-            <div className='cart__section__quantity'>
-                {props.quantity}
-            </div>
-        </div>
+            </td>
+            <td className="total__cell">
+                <div className="total__cell__container">
+                    <div>
+                        {updatedPrice}
+                    </div>
+                    <button className="table__x__button">
+                        X
+                    </button>
+                </div>
+            </td>
+        </tr>
     )
 }
