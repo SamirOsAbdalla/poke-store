cd3220bb39601c512871f9d7c12d1b8765eb63d7
samir01 <samir.os.abdalla@gmail.com>
2022-12-18 10:01:55 -0800

    Each cart section is now part of a table. Decided to group product image and product name together into one cell

--- frontend/src/components/CartSection/CartSection.tsx
+++ frontend/src/components/CartSection/CartSection.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import "./CartSection.css"
 import { useEffect, useState } from 'react'
 import { useAppDispatch } from '../../app/hooks'
-import { removePokemonFromCart } from '../../slices/storedCartPokemon/storedCartPokemon'
-import { decreaseNumberInCart } from '../../slices/numInCart/numInCartSlice'
+import { adjustPokemonInCart, removePokemonFromCart } from '../../slices/storedCartPokemon/storedCartPokemon'
+import { increaseNumberInCart, decreaseNumberInCart } from '../../slices/numInCart/numInCartSlice'
+import { storeNewPokemon } from '../../slices/storedCartPokemon/storedCartPokemon'
 import { QuantityCounter } from '../QuantityCounter/QuantityCounter'
+import { GoPlus } from "react-icons/go"
 
 interface PropType {
     name: string,
@@ -12,19 +14,48 @@ interface PropType {
     quantity: number,
     price: string
 }
+{/*<QuantityCounter name={props.name} setQuantity={(quantity) => { }} initialQuantity={"" + props.quantity} />*/ }
 export const CartSection = (props: PropType) => {
 
     const [updatedPrice, setUpdatedPrice] = useState<string>("")
+    const [currentQuantity, setCurrentQuantity] = useState(props.quantity)
 
 
+    const dispatch = useAppDispatch()
     useEffect(() => {
-        setUpdatedPrice("$" + (parseFloat(props.price.substring(1)) * props.quantity).toFixed(2))
-    })
+        setUpdatedPrice("$" + (parseFloat(props.price.substring(1)) * currentQuantity).toFixed(2))
+    }, [currentQuantity])
 
+    const handleQuantityButtonClick = (increaseQuantity: boolean) => {
+
+        const curQuantity = currentQuantity
+
+        if (increaseQuantity) {
+            setCurrentQuantity(curQuantity + 1)
+            dispatch(increaseNumberInCart(1))
+            dispatch(adjustPokemonInCart({
+                name: props.name,
+                quantity: curQuantity + 1
+            }))
+        } else {
+            setCurrentQuantity(curQuantity - 1)
+            dispatch(decreaseNumberInCart(1))
+            dispatch(adjustPokemonInCart({
+                name: props.name,
+                quantity: curQuantity - 1
+            }))
+        }
+
+    }
+
+    const removePokemonCart = () => {
+        dispatch(removePokemonFromCart(props.name))
+        dispatch(decreaseNumberInCart(currentQuantity))
+    }
 
     return (
         <tr>
-            <td className="product__cell">
+            <td data-label="Product" className="product__cell">
                 <div className="cart__image__container">
                     <img src={props.sprite} className="product__sprite" />
                 </div>
@@ -32,20 +63,26 @@ export const CartSection = (props: PropType) => {
                     {props.name}
                 </div>
             </td>
-            <td className="price__cell">
+            <td data-label="Price: " className="price__cell">
                 {props.price}
             </td>
-            <td className="quantity__cell">
+            <td data-label="Quantity: " className="quantity__cell">
                 <div className="quantity__cell__container">
-                    <QuantityCounter setQuantity={(quantity) => { }} initialQuantity={"" + props.quantity} />
+                    <button onClick={() => handleQuantityButtonClick(true)} className="quantity__cell__button">
+                        <span className="button__span">+</span>
+                    </button>
+                    <div>{currentQuantity}</div>
+                    <button className="quantity__cell__button" onClick={() => handleQuantityButtonClick(false)}>
+                        <span className="button__span">-</span>
+                    </button>
                 </div>
             </td>
-            <td className="total__cell">
+            <td data-label="Total: " className="total__cell">
                 <div className="total__cell__container">
                     <div>
                         {updatedPrice}
                     </div>
-                    <button className="table__x__button">
+                    <button onClick={removePokemonCart} className="table__x__button">
                         X
                     </button>
                 </div>
