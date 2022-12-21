f3e62403abee8b2e4ded23d5845f3000b78ee9a8
samir01 <samir.os.abdalla@gmail.com>
2022-12-21 11:19:01 -0800

    Made decision to add a logout button so that users can logout easily

---
+++ frontend/src/components/LogoutButton/LogoutButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./LogoutButton.css"
+import { useAppDispatch } from '../../app/hooks'
+import { setLoginStatusFalse } from '../../slices/loginStatus/loginStatusSlice'
+export const LogoutButton = () => {
+
+    const dispatch = useAppDispatch()
+    const logoutUser = () => {
+        window.localStorage.setItem("LOGGED_IN", JSON.stringify(false))
+        dispatch(setLoginStatusFalse())
+    }
+
+    return (
+        <button className="logout__button" onClick={logoutUser}>
+            Logout
+        </button>
+    )
+}
