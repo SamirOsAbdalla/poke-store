05876377a99908295974644472140a1450bf6d23
samir01 <samir.os.abdalla@gmail.com>
2022-12-22 14:46:53 -0800

    Logout button now clears the cart in the store and local storage

--- frontend/src/components/LogoutButton/LogoutButton.tsx
+++ frontend/src/components/LogoutButton/LogoutButton.tsx
@@ -2,14 +2,22 @@ import React from 'react'
 import "./LogoutButton.css"
 import { useAppDispatch } from '../../app/hooks'
 import { setLoginStatusFalse } from '../../slices/loginStatus/loginStatusSlice'
-
+import { useNavigate } from 'react-router-dom'
+import { setNumberOfPokemon } from '../../slices/numInCart/numInCartSlice'
+import { removeAllPokemon } from '../../slices/storedCartPokemon/storedCartPokemon'
 
 export const LogoutButton = () => {
 
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
     const logoutUser = () => {
         window.localStorage.setItem("LOGGED_IN", JSON.stringify(false))
+        window.localStorage.setItem("STORED_CART_POKEMON", JSON.stringify([]))
+        window.localStorage.setItem("NUMBER_POKEMON_IN_CART", JSON.stringify(0))
         dispatch(setLoginStatusFalse())
+        dispatch(setNumberOfPokemon(0))
+        dispatch(removeAllPokemon())
+        navigate("/login")
     }
 
     return (
