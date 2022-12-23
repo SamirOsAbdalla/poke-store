79759d0be4672c2aa69d5607781c9029f60d61ea
samir01 <samir.os.abdalla@gmail.com>
2022-12-23 11:17:39 -0800

    Added backend routes so users can alter their favorites

--- backend/controllers/userControllers.ts
+++ backend/controllers/userControllers.ts
@@ -65,6 +65,7 @@ const authUser = asyncHandler(async (req: Request, res: Response) => {
 const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
 
     const user = await User.findById(req.user?._id)
+
     if (user) {
         user.name = req.body.name || user.name
         user.email = req.body.email || user.email
@@ -81,6 +82,7 @@ const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
             email: updatedUser.email,
             name: updatedUser.name,
             pic: updatedUser.pic,
+            favorites: updatedUser.favorites,
             token: generateToken(updatedUser._id),
         })
     } else {
@@ -88,4 +90,55 @@ const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
         throw new Error("User not found")
     }
 })
-module.exports = { registerUser, authUser, updateUserProfile }
+
+const updateUserFavorites = asyncHandler(async (req: Request, res: Response) => {
+    const user = await User.findById(req.user?._id);
+
+    if (user) {
+
+        if (req.body) {
+            user.favorites.push(req.body)
+            const updatedUser = await user.save()
+
+            res.json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                pic: updatedUser.pic,
+                favorites: updatedUser.favorites,
+                token: generateToken(updatedUser._id)
+            })
+        }
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+const removeUserFavorites = asyncHandler(async (req: Request, res: Response) => {
+    const user = await User.findById(req.user?._id);
+
+    if (user) {
+
+        if (req.body) {
+
+            user.favorites = user.favorites.filter((favorite: any) => {
+                return favorite.name.toLowerCase() !== req.body.name.toLowerCase()
+            })
+            const updatedUser = await user.save()
+
+            res.json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                pic: updatedUser.pic,
+                favorites: updatedUser.favorites,
+                token: generateToken(updatedUser._id)
+            })
+        }
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+module.exports = { registerUser, authUser, updateUserProfile, updateUserFavorites, removeUserFavorites }
--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,9 +1,12 @@
 const express = require("express")
-const { registerUser, authUser, updateUserProfile } = require("../controllers/userControllers")
+const { registerUser, authUser, updateUserProfile, updateUserFavorites, removeUserFavorites } = require("../controllers/userControllers")
 const router = express.Router()
 const { protect } = require("../middlewares/authMiddleware")
 
 router.route("/").post(registerUser)
 router.route("/login").post(authUser)
 router.route("/profile").post(protect, updateUserProfile)
+router.route("/profile/favorites/add").post(protect, updateUserFavorites)
+router.route("/profile/favorites/remove").post(protect, removeUserFavorites)
+
 module.exports = router
